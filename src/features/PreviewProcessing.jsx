e60489f129b8736907bcf196b235abe7cc972786
arcjun01 <arciaga.juno@student.greenriver.edu>
2025-11-19 10:54:10 -0800

    Implemented PreviewProcessing.jsx file

---
+++ src/features/PreviewProcessing.jsx
@@ -0,0 +1,23 @@
+import Header from '../components/Header'
+import Buttons from '../components/Buttons'
+
+export default function PreviewProcessing() {
+    return (
+
+
+        <div>
+            <Header />
+
+            <h1>Original Frame</h1>
+            <h1>Binarized Frame</h1>
+            <h2>Target Color</h2>
+            <h2>Threshold</h2>
+
+            <Buttons />
+
+        </div>
+
+
+        
+    )
+}
