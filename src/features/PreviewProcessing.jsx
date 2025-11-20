5ed7aefd338c8cffb5d0099efbbce8b333984780
arcjun01 <arciaga.juno@student.greenriver.edu>
2025-11-20 13:59:42 -0800

    Added a new folder to store all the css files and added import in PreviewProcessing file.

--- src/features/PreviewProcessing.jsx
+++ src/features/PreviewProcessing.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
 import Header from '../components/Header';
+import '../css/previewProcessing.css';
 
 export default function PreviewProcessing() {
     const navigate = useNavigate();
